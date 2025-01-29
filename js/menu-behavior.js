ec2cc3f71c00b4cecdbf2a61c448d44a02507a20
mari0ar <mario.artaz@venex.com.ar>
2025-01-28 21:48:47 -0300

    mejora en la responsividad y el iframe, agrego iconos en las tarjetas

--- js/menu-behavior.js
+++ js/menu-behavior.js
@@ -18,11 +18,31 @@ function deactivateMenu() {
 }
 
 // Smooth scroll al clickear opciones del menú
+// document.querySelectorAll('.navbar a, .footer a').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         const target = document.querySelector(this.getAttribute('href'));
+//         target.scrollIntoView({ behavior: 'smooth' });
+//         deactivateMenu()
+//     });
+// });
+
 document.querySelectorAll('.navbar a, .footer a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
+        const href = this.getAttribute('href');
+
+        // Permitir enlaces externos o aquellos con target="_blank"
+        if (this.target === '_blank' || href.startsWith('http') || href === '#') {
+            return; // Salir sin bloquear el comportamiento predeterminado
+        }
+
+        // Evitar el comportamiento predeterminado solo para anclas internas
         e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        target.scrollIntoView({ behavior: 'smooth' });
-        deactivateMenu()
+        const target = document.querySelector(href);
+
+        if (target) {
+            target.scrollIntoView({ behavior: 'smooth' });
+            deactivateMenu();
+        }
     });
 });
