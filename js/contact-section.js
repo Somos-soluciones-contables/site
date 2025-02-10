398becad89b465d40471cfc946a3626d3cdcfd96
mari0ar <mario.artaz@venex.com.ar>
2025-02-09 21:46:39 -0300

    Ajustes para el contact form

--- js/contact-section.js
+++ js/contact-section.js
@@ -15,21 +15,3 @@ function createContactForm() {
 }
 window.onload = createContactForm;
 
-//Validación del campos del form
-document.querySelector("form").addEventListener("submit", function(event) {
-    let valid = true;
-    const inputs = document.querySelectorAll("input, textarea");
-    
-    inputs.forEach(input => {
-        if (!input.value) {
-            valid = false;
-            input.style.borderColor = "#fe0419"; 
-        } else {
-            input.style.borderColor = ""; 
-        }
-    });
-
-    if (!valid) {
-        event.preventDefault();
-    }
-});
